6b413f156b7a1cff35c00a2f729554d91d2a0280
hackhsz <zuoming@Shizhong.local>
2017-04-18 18:12:46 -0400

    change

--- final.py
+++ final.py
@@ -13,6 +13,10 @@ from collections import Counter
 from collections import OrderedDict
 
 
+
+
+
+
 class Movie(object):
     
     
@@ -20,20 +24,27 @@ class Movie(object):
         self.title = dict1["Title"]
         self.director = dict1["Director"]
         self.imdb = dict1["imdbRating"]
-        listactors = [x.strip() for x in dict1["Actors"].split(',')]
+        act = dict1["Actors"]
+        self.acts = [x.strip() for x in act.split(',')]
+        listactors = [x.strip() for x in act.split(',')]
         self.actors = listactors[0]
         self.id = dict1["imdbID"]
         self.language = dict1["Language"]
         self.country = dict1["Country"]
+    
     def __str__(self):
         return self.title
     
     def list_of_actors(self):
-        return [x.strip() for x in dict1["Actors"].split(',')]
+        listone = self.acts
+        return [x.strip() for x in listone.split(',')]
 
     def number_of_language(self):
         listone = self.language
         return len([x.strip() for x in listone.split(',')])
+
+
+
 
 consumer_key = twitter_info.consumer_key
 consumer_secret = twitter_info.consumer_secret
@@ -130,28 +141,20 @@ for item in movies:
     #pdb.set_trace()
     for tweet in tweets:
         tweet["movie_id"]= item.id
-    #tweets[len(tweets)] = item.id
+    
     tweetlist.append(tweets)
 # The above is saved to be as the the list of tweets
 
-#pdb.set_trace()
 
-#print(tweetlist[0])
 # Invocation to my User Twitter Function 
 
 userlist = []
-
 tweets=[]
 
 for item in tweetlist:
- #   pdb.set_trace()
-#    print(item.keys())
-#    print(len(item))
+ 
     
-    #movieid = item["movie_id"]
     for tweet in item:
-#        pdb.set_trace()
- #       print(tweet["movie_id"])
         dicttw = {}
         #print(tweet.keys())
         dicttw["tweet_id"] = tweet["id"]
@@ -176,11 +179,7 @@ for item in tweetlist:
             dicttemp["description"] = user["description"]
             dicttemp["user_id"] = user["id_str"]
             userlist.append(dicttemp)
-#pdb.set_trace()
-#print(userlist[0].keys())
-#print(userlist[0])
-#print(tweets[0])
-    
+#pdb.set_tr
 #Create Table for Tweets
 
 conn = sqlite3.connect('final_tweets.db')
@@ -283,7 +282,7 @@ def makequery():
     query = "SELECT SUM(Tweets.retweets),Movies.title FROM Tweets INNER JOIN Movies ON Tweets.movie_id = Movies.movie_id GROUP BY Tweets.movie_id"
     cur.execute(query)
     listone = cur.fetchall()
-    pdb.set_trace()
+#    pdb.set_trace()
     title_movie = sorted(listone)[0][1]
     #pdb.set_trace()
     print(title_movie)
@@ -314,3 +313,115 @@ def makequery():
 
 
 makequery()
+
+
+
+
+
+
+
+# Test Area
+
+
+
+print("\n\nBELOW THIS LINE IS OUTPUT FROM TESTS:\n")
+
+
+class Constructor1(unittest.TestCase):
+    dict1 = dict([('Actors',['adf','hsz','zhong','bob']),('Title',"the great Zhong"),('imdbRating',1.5),('imdbID',123123),('Country','China'),('Language','Chinese'),('Director','Shan')])
+        
+    def test_constructor(self):
+        dict1 = dict([('Actors',['adf','hsz','zhong','bob']),('Title',"the great Zhong"),('imdbRating',1.5),('imdbID',123123),('Country','China'),('Language','Chinese'),('Director','Shan')])
+        s = Movie(dict1)
+        self.assertEqual(s.title,"the great Zhong")
+    def test_str(self):
+        dict1 = dict([('Actors',['adf','hsz','zhong','bob']),('Title',"the great Zhong"),('imdbRating',1.5),('imdbID',123123),('Country','China'),('Language','Chinese'),('Director','Shan')])
+        m = Movie(dict1)
+        self.assertEqual(print(m),"the great Zhong")
+    def test_imdb(self):
+        dict1 = dict([('Actors',['adf','hsz','zhong','bob']),('Title',"the great Zhong"),('imdbRating',1.5),('imdbID',123123),('Country','China'),('Language','Chinese'),('Director','Shan')])
+
+        l = Movie(dict1)
+        self.assertEqual(l.imdb,1.5)
+    def test_country(self):
+
+        dict1 = dict([('Actors',['adf','hsz','zhong','bob']),('Title',"the great Zhong"),('imdbRating',1.5),('imdbID',123123),('Country','China'),('Language','Chinese'),('Director','Shan')])
+        p = Movie(dict1)
+        self.assertEqual(p.country,"China")
+    
+    def test_director(self):
+        dict1 = dict([('Actors',['adf','hsz','zhong','bob']),('Title',"the great Zhong"),('imdbRating',1.5),('imdbID',123123),('Country','China'),('Language','Chinese'),('Director','Shan')])
+        q = Movie(dict1)
+        self.assertEqual(q.director,'Shan')
+    def test_num_of_language(self):
+        dict1 = dict([('Actors',['adf','hsz','zhong','bob']),('Title',"the great Zhong"),('imdbRating',1.5),('imdbID',123123),('Country','China'),('Language','Chinese'),('Director','Shan')])
+        w = Movie(dict1)
+        self.assertTrue(w.number_of_language()==1)
+    def test_list_of_actors(self):
+
+        dict1 = dict([('Actors',['adf','hsz','zhong','bob']),('Title',"the great Zhong"),('imdbRating',1.5),('imdbID',123123),('Country','China'),('Language','Chinese'),('Director','Shan')])
+        e = Movie(dict1)
+        self.assertTrue(len(e.list_of_actors())==4)
+
+class Task2(unittest.TestCase):
+    def test_tweets_1(self):
+        conn = sqlite3.connect('final_tweets.db')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Tweets');
+        result = cur.fetchall()
+        self.assertTrue(len(result)>=20, "Testing there are at least 20 records in the Tweets database")
+        conn.close()
+    def test_tweets_2(self):
+        conn = sqlite3.connect('final_tweets.db')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Tweets');
+        result = cur.fetchall()
+        self.assertTrue(len(result[1])==6, "Testing there are 6 columns in the Tweets database")
+        conn.close()
+    def test_tweets_3(self):
+        conn = sqlite3.connect('final_tweets.db')
+        cur = conn.cursor()
+        cur.execute('SELECT user_posted FROM Tweets');
+        result = cur.fetchall()
+        self.assertTrue(len(result[1][0])>=2,"Testing that a tweet user_id value fulfills a requirement of being a Twitter user id rather than an integer, etc")
+        conn.close()
+    def test_tweets_4(self):
+        conn = sqlite3.connect('final_tweets.db')
+        cur = conn.cursor()
+        cur.execute('SELECT tweet_id FROM Tweets');
+        result = cur.fetchall()
+        self.assertTrue(result[0][0] != result[19][0], "Testing part of what's expected such that tweets are not being added over and over (tweet id is a primary key properly)...")
+        if len(result) > 20:
+            self.assertTrue(result[0][0] != result[20][0])
+        conn.close()
+    
+    def test_user_1(self):
+        conn = sqlite3.connect('final_tweets.db')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Users');
+        result = cur.fetchall()
+        self.assertTrue(len(result)>=20, "Testing there are at least 20 records in the Users database")
+        conn.close()
+    def test_tweets_2(self):
+        conn = sqlite3.connect('final_tweets.db')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Users');
+        result = cur.fetchall()
+        self.assertTrue(len(result[0])==4, "Testing there are 4 columns in the Users database")
+        conn.close()
+    def test_movie_1(self):
+        conn = sqlite3.connect('final_tweets.db')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Movies');
+        result = cur.fetchall()
+        self.assertTrue(len(result)==3, "Testing there are 3 records in the Movie database")        
+    def test_movie_2(self):
+        conn = sqlite3.connect('final_tweets.db')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Movies');
+        result = cur.fetchall()
+        self.assertTrue(len(result[0])==7, "Testing there are 7 column in the Movie database")
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
